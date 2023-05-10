4692340d839a3bd1eed0fb0537d33d2c325ffb30
Ella <ella.code.j@gmail.com>
2023-05-11 01:06:02 +0900

    thymleaf connect

--- src/main/java/com/test/SpringBootApi/SpringBootApiApplication.java
+++ src/main/java/com/test/SpringBootApi/SpringBootApiApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApiApplication { public static void main(String[] args) {
+public class SpringBootApiApplication {
+	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
 }
--- src/main/java/com/test/SpringBootApi/service/ProductService.java
+++ src/main/java/com/test/SpringBootApi/service/ProductService.java
@@ -6,9 +6,9 @@ import java.util.Optional;
 
 public interface ProductService {
 
-    public Product save(Product product);
-    public Optional<Product> findById(Long id);
-    public Product update(Long id, Product product);
-    public void delete(Long id);
+    Product save(Product product);
+    Optional<Product> findById(Long id);
+    Product update(Long id, Product product);
+    void delete(Long id);
 
 }
